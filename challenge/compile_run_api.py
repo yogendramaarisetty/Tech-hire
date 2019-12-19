76bdd2b4230a6615ce34006586ce9ede78221b18
yogendramaarisetty <ymaarisetty@hostanalytics.com>
2019-12-19 20:13:20 +0530

    cdedd

--- challenge/compile_run_api.py
+++ challenge/compile_run_api.py
@@ -8,13 +8,13 @@ def compile_run(language,code,custom_input,request):
     if language == "Python":
         output = execute_python(code,custom_input,file_name,path)
     elif language == "Java":
-        output = execute_java(code,custom_input,file_name)
+        output = execute_java(code,custom_input,file_name,path)
     elif language == "C":
         output = execute_c(code,custom_input,file_name,path)
     elif language == "C++":
         output = execute_cpp(code,custom_input,file_name,path)
     elif language == "C#":
-        output = execute_csharp(code,custom_input,file_name)
+        output = execute_csharp(code,custom_input,file_name,path)
     return output
 
 def execute_python(code,custom_input,file_name,path):
@@ -44,8 +44,39 @@ def execute_python(code,custom_input,file_name,path):
     else:
         os.chdir(root_path)
         return errors   
-def execute_java(code,custom_input,file_name):
-    return "this is java output"
+
+def execute_java(code,custom_input,file_name,path):
+    root_path = os.getcwd()
+    path= os.path.join(path,'java_codes')
+    os.chdir(f'{path}')
+    try:
+        os.mkdir(file_name)
+    except:
+        pass
+    path= os.path.join(path,file_name)
+    os.chdir(path)
+    file_name_ext= "MyClass.java"
+    code_file = open(f'{file_name_ext}','w')
+    code_file.flush()
+    file_lines = code.split("\n")
+    for l in file_lines:
+        code_file.write(l+"\n")
+    code_file.close()
+    cmd = f'g++ {file_name_ext}'
+    p=os.getcwd()
+    compile_code = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    errors = compile_code.stderr.readlines()
+    if len(errors)==0:
+        run_code = subprocess.run("a", stdout=PIPE,input=custom_input, stderr=subprocess.PIPE,encoding='ascii',shell=True)
+        os.chdir(root_path)
+        return run_code.stdout
+    else:
+        error=""
+        for e in errors:
+            error+=e.decode("utf-8")
+        os.chdir(root_path)
+        return error 
+
 
 def execute_c(code,custom_input,file_name,path):
     root_path = os.getcwd()
@@ -112,4 +143,33 @@ def execute_cpp(code,custom_input,file_name,path):
         return error 
 
 def execute_csharp(code,custom_input,file_name):
-    return "this is C# output"
+    root_path = os.getcwd()
+    path= os.path.join(path,'csharp_codes')
+    os.chdir(f'{path}')
+    try:
+        os.mkdir(file_name)
+    except:
+        pass
+    path= os.path.join(path,file_name)
+    os.chdir(path)
+    file_name_ext= file_name+".cs"
+    code_file = open(f'{file_name_ext}','w')
+    code_file.flush()
+    file_lines = code.split("\n")
+    for l in file_lines:
+        code_file.write(l+"\n")
+    code_file.close()
+    cmd = f'g++ {file_name_ext}'
+    p=os.getcwd()
+    compile_code = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    errors = compile_code.stderr.readlines()
+    if len(errors)==0:
+        run_code = subprocess.run("a", stdout=PIPE,input=custom_input, stderr=subprocess.PIPE,encoding='ascii',shell=True)
+        os.chdir(root_path)
+        return run_code.stdout
+    else:
+        error=""
+        for e in errors:
+            error+=e.decode("utf-8")
+        os.chdir(root_path)
+        return error 
